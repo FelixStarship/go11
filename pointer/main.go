2e7a7d9c4b0f50dd60e4e72e5dae5595b8ac952e
huxz01 <huxz01@mingyuanyun.com>
2022-07-25 16:46:50 +0800

    指针

--- pointer/main.go
+++ pointer/main.go
@@ -9,5 +9,16 @@ func main() {
 	fmt.Println(p0)
 	fmt.Println(*p0)
 	fmt.Println(&p0)
+	x := *p0
 
+	fmt.Println(&*p0)
+	x = 100
+
+	p1, p2 := &p0, &p0
+	fmt.Println(x)
+	fmt.Println("p1=", p1)
+	fmt.Println("p2=", p2)
+	fmt.Println("p1=", *p1)
+	fmt.Println("p2=", *p2)
+	//fmt.Println(p0==p1)
 }
