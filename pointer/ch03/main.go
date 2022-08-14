37d3e40632224336be69d619ed33e46a7a2274a4
Felix <2793800263@qq.com>
2022-08-14 10:56:09 +0800

    temporal工作流部署文档

---
+++ pointer/ch03/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type (
+	GitLogin struct {
+		Account   string
+		Address   string
+		LoginTime time.Time
+		Project   []struct {
+			Name   *string
+			ExTime time.Time
+			Auther string
+		}
+	}
+)
+
+func main() {
+	str := "Felix"
+	str1 := "gitlab"
+	gitLogin := GitLogin{
+		Account:   "Felix",
+		Address:   "github",
+		LoginTime: time.Now(),
+		Project: []struct {
+			Name   *string
+			ExTime time.Time
+			Auther string
+		}{{Name: &str, ExTime: time.Now(), Auther: "test"}},
+	}
+
+	gitLogin.Project[0].Name = &str1
+	fmt.Println(gitLogin.Project[0].Name)
+	//for _, item := range gitLogin.Project {
+	//	item.Name = &str1
+	//	fmt.Println(item.Name)
+	//	fmt.Println(gitLogin.Project[0].Name)
+	//}
+	fmt.Println(*gitLogin.Project[0].Name)
+}
