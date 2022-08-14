3b8411cd2b0bad640f014a177b18d4d1d79f0fc9
Felix <2793800263@qq.com>
2022-08-14 13:33:09 +0800

    容器类型

--- array/ch01/main.go
+++ array/ch01/main.go
@@ -2,8 +2,30 @@ package main
 
 import "fmt"
 
-func main()  {
-	array:=[...]bool{true,false}
-	fmt.Println(array)
+type st struct {
+	Name string
+	Agt  []int
 }
 
+func main() {
+	array := [...]bool{true, false}
+	fmt.Println(array)
+
+	array1 := [1]int{}
+	fmt.Println(array1)
+	st1 := st{}
+	fmt.Println(st1)
+
+	pm := &map[string]int{"C": 1}
+	fmt.Printf("%T\n", pm)
+
+	st2 := st{Name: "felix", Agt: []int{1, 2, 3}}
+	fmt.Println(st2)
+	st2.oprtation()
+	fmt.Println(st2)
+}
+
+func (s *st) oprtation() {
+	s.Name = "指针"
+	s.Agt = []int{4, 5, 6}
+}
--- pointer/ch03/main.go
+++ pointer/ch03/main.go
@@ -11,7 +11,7 @@ type (
 		Address   string
 		LoginTime time.Time
 		Project   []struct {
-			Name   *string
+			Name   string
 			ExTime time.Time
 			Auther string
 		}
@@ -21,23 +21,19 @@ type (
 func main() {
 	str := "Felix"
 	str1 := "gitlab"
-	gitLogin := GitLogin{
+	gitLogin := &GitLogin{
 		Account:   "Felix",
 		Address:   "github",
 		LoginTime: time.Now(),
 		Project: []struct {
-			Name   *string
+			Name   string
 			ExTime time.Time
 			Auther string
-		}{{Name: &str, ExTime: time.Now(), Auther: "test"}},
+		}{{Name: str, ExTime: time.Now(), Auther: "test"}},
 	}
 
-	gitLogin.Project[0].Name = &str1
+	gitLogin1 := gitLogin
+	gitLogin1.Project[0].Name = str1
 	fmt.Println(gitLogin.Project[0].Name)
-	//for _, item := range gitLogin.Project {
-	//	item.Name = &str1
-	//	fmt.Println(item.Name)
-	//	fmt.Println(gitLogin.Project[0].Name)
-	//}
-	fmt.Println(*gitLogin.Project[0].Name)
+	fmt.Println(gitLogin1.Project[0].Name)
 }
